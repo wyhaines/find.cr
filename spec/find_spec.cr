64733f563f722f58e1606641ea14aeadad04a5b9
Kirk Haines <wyhaines@gmail.com>
2021-03-01 11:50:15 -0700

    Make Specs into happy Specs.

--- spec/find_spec.cr
+++ spec/find_spec.cr
@@ -1,15 +1,39 @@
 require "./spec_helper"
 require "uuid"
+require "file_utils"
+
+def make_tmpdir(path : String? | Path = nil)
+  dirname = nil
+  if path.nil?
+    dirname = File.join(Dir.tempdir, UUID.random.to_s)
+  else
+    dirname = File.join(Dir.tempdir, path)
+  end
+  Dir.mkdir dirname
+  dirname
+rescue
+  nil
+end
+
+def with_tmpdir(path : Nil | String | Path = nil, &blk : String ->)
+  tempdir = make_tmpdir(path)
+  if !tempdir.nil? && File.exists? tempdir
+    Dir.open(tempdir) { blk.call(tempdir) }
+  end
+ensure
+  # FileUtils.rm_rf(tmpdir) if tmpdir && tmpdir.open?
+  FileUtils.rm_rf tempdir if !tempdir.nil? && File.exists?(tempdir) && File.directory?(tempdir)
+end
 
 describe Find do
   it "works on an empty directory" do
-    begin
-      tempdir = Dir.mkdir File.join(Dir.tempdir, UUID.random.to_s)
+    with_tmpdir do |tempdir|
       a = [] of String?
-      Find.find(tempdir) {|f| a << f}
+      Find.find(tempdir) { |f| a << f }
       a.should eq [tempdir]
-    ensure
-      tempdir.close if tempdir && tempdir.open?
     end
+  end
+
+  it "works if asked to find something that does not exist" do
   end
 end
